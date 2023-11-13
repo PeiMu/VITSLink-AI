4abc32c5eecbf8d2acade2730fa3d3bbb201793f
Pei Mu <ds1231h@126.com>
2023-11-13 15:35:19 +0000

    minor fix

--- run.sh
+++ run.sh
@@ -8,16 +8,16 @@ if [ "${option}" == 1 ]; then
   echo Starting English to Chinese Text to Speech Translator
   echo Starting VITS...
   cd vits-models || exit
-  python app.py --api
+  /usr/bin/python3.10 app.py --api
   cd ..
-  python english_to_chinese.py
+  /usr/bin/python3.10 english_to_chinese.py
 elif [ "${option}" == 2 ]; then
   echo Starting English to Japanese Text to Speech Translator
   echo Starting VITS...
   cd vits-models || exit
-  python app.py --api
+  /usr/bin/python3.10 app.py --api
   cd ..
-  python english_to_japanese.py
+  /usr/bin/python3.10 english_to_japanese.py
 else
   echo Invalid selection. Please try again.
 fi
